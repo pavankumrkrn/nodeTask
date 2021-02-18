885fd4e7fc1252f735503741eff4c629a10a957f
Krishna Pavani <kplollipop1@gmail.com>
2021-02-18 13:02:30 +0530

    final

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 
-console.log(fs.readdirSync('./example'))
+let arr = fs.readdirSync('./example');
+arr.forEach(i => {
+    let f = fs.statSync("./example/" + i)
+    console.log(f.isFile())
+})
 
 fs.readFile('index.txt', 'utf8', (err, data) => {
     console.log(data)
